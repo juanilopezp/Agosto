48be990062dd8a65f004b95ae237216a025dce19
juanilopezp <juanilopezpatterson@gmail.com>
2023-08-02 02:19:21 -0300

    finally

--- Ecommerce-App-master/src/front/AddProduct.js
+++ Ecommerce-App-master/src/front/AddProduct.js
@@ -2,13 +2,31 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import './AddProduct.css'
 
-
 function CreateProduct() {
     const [nombre, setNombre] = useState("")
     const [image, setImage] = useState("")
     const [precio, setPrecio] = useState(0)
     const [descripcion, setDescripcion] = useState("")
 
+    const uploadImage = async (e) => {
+        const file = e.target.files[0];
+        const base64 = await convertBase64(file);
+        setImage(base64)
+      };
+      const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file);
+    
+          fileReader.onload = () => {
+            resolve(fileReader.result);
+          };
+    
+          fileReader.onerror = (error) => {
+            reject(error);
+          };
+        });
+      };
     const addProduct = () =>{
 
         axios.post('http://localhost:5000/products',{
@@ -25,9 +43,11 @@ function CreateProduct() {
   return (
     
         <>
-        <div>
+        <div className='form-container'>
             <input onChange={(e) => setNombre(e.target.value)} value={nombre} placeholder='Titulo'/>
-            <input onChange={(e) => setImage(e.target.value)} value={image} placeholder='imagen 64'/>
+            <input className='input-file' type='file' onChange={(e) => {
+          uploadImage(e);
+        }} placeholder='imagen 64'/>
             <input onChange={(e) => setDescripcion(e.target.value)} value={descripcion} placeholder='descripcion'/>
             <input type="number" onChange={(e) => setPrecio(e.target.value)} value={precio} />
             <button onClick={addProduct}>subir producto</button>
